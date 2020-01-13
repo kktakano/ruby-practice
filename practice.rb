1758dbec6a615b1b87c465bd8bfa76c440abba62
kktakano <kktakano03@gmail.com>
2020-01-13 18:46:13 +0900

    Update practice.rb

--- practice.rb
+++ practice.rb
@@ -95,3 +95,20 @@
 #   r << i * input_line
 # end
 # puts r.join(" ")
+
+
+
+
+####
+# # ある正の整数aとbがスペース区切りで入力されます。
+# # aとbを比較し大きい方の値を出力して下さい。aとbが同じ場合は「eq」と出力して下さい。
+# input_line = gets.split(" ")
+# a = input_line[0].to_i
+# b = input_line[1].to_i
+# if a == b
+#   puts "eq"
+# elsif a > b
+#   puts a
+# else
+#   puts b
+# end
